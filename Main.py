be0b75602312e8d7e42c5f4881c32655011eb33f
Wesley Breno <74777926+Wesley-Breno@users.noreply.github.com>
2021-06-26 00:16:09 -0300

    2 updates
    * Fiz um (while True) dentro do primeiro (while True) para o algoritmo nao ter que voltar para a tela inicial.
    * Escrevi o algoritmo de (informaçoes) para caso o usuario execute ele no menu.

--- Main.py
+++ Main.py
@@ -4,11 +4,12 @@ while True:
     titulo_inicial()
     press_enter()
     # Menu inicial
-    pular(10)
-    print(f'\033[;1m{"Menu Principal":^50}\033[m')
-    pular(2)
-    try:
-        escolha = int(input("""Digite o numero de uma das opcões abaixo;
+    while True:
+        pular(10)
+        print(f'\033[;1m{"Menu Principal":^50}\033[m')
+        pular(2)
+        try:
+            escolha = int(input("""Digite o numero de uma das opcões abaixo;
 
 
 1 \033[1;31m➤\033[m Iniciar
@@ -20,14 +21,27 @@ while True:
 
 
 Escolha: """))
-    except:
-        erro(msg='Digite um dos valores\ninformados na tela.')
-    else:
-        if escolha == 1:
-            print('iniciou')
-        elif escolha == 2:
-            print('Informacões')
-        elif escolha == 3:
-            print('Saiu')
-        else:
+        except:
             erro(msg='Digite um dos valores\ninformados na tela.')
+            pular(17)
+        else:
+            if escolha == 1:
+                print('iniciou')
+            elif escolha == 2:
+                pular(11)
+                print(f'\033[;1m{"Informacões":^50}\033[m')
+                pular(3)
+                print("""\033[1;31m▰\033[m Calculator
+    É um jogo educacional para treinar sua matematica basica.
+            
+\033[1;31m▰\033[m Modos
+    Soma, Subtracão e Divisão.
+    
+\033[1;31m▰\033[m Em breve novas atualizacões.""")
+                press_enter(c=1, msg='voltar')
+                pular(17)
+            elif escolha == 3:
+                print('Saiu')
+            else:
+                erro(msg='Digite um dos valores\ninformados na tela.')
+                pular(17)
